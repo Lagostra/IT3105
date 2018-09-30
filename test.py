f15a0f2b0d7120e57dc90c2f9eabc5188cd152e2
Eivind Lie Andreassen <ela4991@gmail.com>
2018-09-30 13:23:35 +0200

    Clean up test file

--- test.py
+++ test.py
@@ -2,14 +2,22 @@ from network import Network
 from layers import Dense, DenseSequence, Dropout
 import tensorflow as tf
 import tflowtools as tft
-import numpy as np
 
 layers = [
-    DenseSequence([10, 8, 10], activation_function='sigmoid'),
+    DenseSequence([101, 25, 1], activation_function='sigmoid'),
 ]
 
-network = Network(layers, tft.gen_all_one_hot_cases(10), minibatch_size=8, steps=2500, loss_function='cross_entropy',
-                  validation_fraction=-1, test_fraction=-1, validation_interval=5, output_functions='argmax_one_hot')
+network = Network(
+    layers,
+    tft.gen_symvect_cases(101, 2000),
+    minibatch_size=100,
+    steps=2000,
+    loss_function='mse',
+    test_fraction=0.1,
+    validation_fraction=0.1,
+    validation_interval=50,
+    output_functions=[tf.round]
+)
 
 network.build()
 network.train(plot_results=True)
