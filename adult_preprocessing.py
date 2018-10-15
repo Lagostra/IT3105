94c043a75d208ed1f0c79a67a524b78db16a6f92
Eivind Lie Andreassen <ela4991@gmail.com>
2018-10-15 08:48:19 +0200

    Remove unused script files

--- adult_preprocessing.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv('data/adult_raw.txt', header=None)
-for i in [1, 2, 3, 5, 6, 7, 8, 9, 10, 13, 14]: #[1, 3, 5, 6, 7, 8, 9, 13, 14]:
-    data[i] = pd.Categorical(data[i])
-    data[i] = data[i].cat.codes
-print(data)
-data.to_csv('data/adult.txt', index=False, header=False)
